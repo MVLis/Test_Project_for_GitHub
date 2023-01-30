193061a3620f07a786c76b6fd825724fdd8f6072
Marina Lis <mvlis201190@gmail.com>
2023-01-30 16:25:18 +0300

    feat: create simple calculator

--- Test Project for GitHub/Program.cs
+++ Test Project for GitHub/Program.cs
@@ -6,7 +6,41 @@ namespace Test_Project_for_GitHub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.WriteLine("Первое число.");
+                var firstNumber = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Математическая операция:\n +\n -\n *\n /\n");
+                var operation = Console.ReadLine();
+
+                Console.WriteLine("Второе число.");
+                var secondNumber = Convert.ToDouble(Console.ReadLine());
+
+                double result;
+                switch (operation)
+                {
+                    case "+":
+                        result = firstNumber + secondNumber;
+                        Console.WriteLine("Результат: " + result);
+                        break;
+                    case "-":
+                        result = firstNumber - secondNumber;
+                        Console.WriteLine("Результат: " + result);
+                        break;
+                    case "*":
+                        result = firstNumber * secondNumber;
+                        Console.WriteLine("Результат: " + result);
+                        break;
+                    case "/":
+                        result = firstNumber / secondNumber;
+                        Console.WriteLine("Результат: " + result);
+                        break;
+                    default:
+                        Console.WriteLine("Выбрана неверная операция.");
+                        break;
+                }
+            }
         }
     }
 }
